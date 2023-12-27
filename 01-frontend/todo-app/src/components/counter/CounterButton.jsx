864c019c1201cd140d035796368a43fb87669e3e
jeong <chung_junhuck@infonic.co.jp>
2023-12-27 22:57:48 +0900

    refactor, end of Counter function

--- 01-frontend/todo-app/src/components/counter/CounterButton.jsx
+++ 01-frontend/todo-app/src/components/counter/CounterButton.jsx
@@ -1,31 +1,15 @@
-import { useState } from 'react';
 import { PropTypes } from 'prop-types';
 
 export default function CounterButton({by, incrementMethod, decrementMethod}) {
-
-  // [value, function]
-  // const array = [1, 2]
-  // const [firstElt, secondElt] = array
-  const [count, setCount] = useState(0);
-
-  function incrementCounterButtonFunction() {
-    setCount(count + by);
-    incrementMethod(by);
-  }
-
-  function decresementCounterButtonFunction(){
-    setCount(count - by);
-    decrementMethod(by);
-  }
 
   return (
     <div className="CounterButton">
       <div>
         <button className="counterButton"
-                onClick={incrementCounterButtonFunction}
+                onClick={() => incrementMethod(by)}
         >+{by}</button>
         <button className="counterButton"
-                onClick={decresementCounterButtonFunction}
+                onClick={() => decrementMethod(by)}
         >-{by}</button>
       </div>
     </div>

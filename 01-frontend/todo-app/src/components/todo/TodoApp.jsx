e8ec869ef0af38cb8e6d84b74fa9ec89e575c28e
jeong <chung_junhuck@infonic.co.jp>
2023-12-27 23:09:28 +0900

    TodoApp skeleton

---
+++ 01-frontend/todo-app/src/components/todo/TodoApp.jsx
@@ -0,0 +1,39 @@
+export default function TodoApp() {
+  return (
+    <div className="TodoApp">
+      <LoginComponent />
+      {/* <WelcomeComponent /> */}
+    </div>
+  )
+}
+
+function LoginComponent() {
+  return (
+    <div className="Login">
+      <div className="LoginForm">
+        <div>
+          <label>User Name</label>
+          <input type="text" name="username" />
+        </div>
+        <div>
+          <label>Password</label>
+          <input type="password" name="password" />
+        </div>
+        <div>
+          <button type="button" name="login"></button>
+        </div>
+      </div>
+      username
+      password
+      button
+    </div>
+  )
+}
+
+function WelcomeComponent() {
+  return (
+    <div className="Welcome">
+      Welcome Component
+    </div>
+  )
+}

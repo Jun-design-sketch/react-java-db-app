22b9e9954bd7389aae0768e6d20aa6d21b72e7ba
jeong <chung_junhuck@infonic.co.jp>
2023-12-29 15:35:18 +0900

    change login component

--- 01-frontend/todo-app/src/components/todo/TodoApp.jsx
+++ 01-frontend/todo-app/src/components/todo/TodoApp.jsx
@@ -1,3 +1,6 @@
+import { useState } from 'react'
+import './TodoApp.css'
+
 export default function TodoApp() {
   return (
     <div className="TodoApp">
@@ -8,24 +11,33 @@ export default function TodoApp() {
 }
 
 function LoginComponent() {
+
+  const [username, setUsername] = useState('in28minutes')
+  const [password, setPassword] = useState('')
+
+  function handleUsernameChange(event) {
+    setUsername(event.target.value)
+  }
+
+  function handlePasswordChange(event) {
+    setPassword(event.target.value)
+  }
+
   return (
     <div className="Login">
       <div className="LoginForm">
         <div>
           <label>User Name</label>
-          <input type="text" name="username" />
+          <input type="text" name="username" value={username} onChange={handleUsernameChange}/>
         </div>
         <div>
           <label>Password</label>
-          <input type="password" name="password" />
+          <input type="password" name="password" value={password} onChange={handlePasswordChange}/>
         </div>
         <div>
-          <button type="button" name="login"></button>
+          <button type="button" name="login">Login</button>
         </div>
       </div>
-      username
-      password
-      button
     </div>
   )
 }

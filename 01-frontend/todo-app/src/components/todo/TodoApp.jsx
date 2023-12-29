28a9bbe218debb0082bec7d9432b54161cc9b060
jeong <chung_junhuck@infonic.co.jp>
2023-12-30 00:26:07 +0900

    Hard Coding: authentification, renewed package.json and reinstalled NodeJS

--- 01-frontend/todo-app/src/components/todo/TodoApp.jsx
+++ 01-frontend/todo-app/src/components/todo/TodoApp.jsx
@@ -15,6 +15,9 @@ function LoginComponent() {
   const [username, setUsername] = useState('in28minutes')
   const [password, setPassword] = useState('')
 
+  const [showSuccessMessage, setShowSeccessMessage] = useState(false)
+  const [showErrorMessage, setShowErrorMessage] = useState(false)
+
   function handleUsernameChange(event) {
     setUsername(event.target.value)
   }
@@ -23,8 +26,42 @@ function LoginComponent() {
     setPassword(event.target.value)
   }
 
+  function handelSubmit(event){
+    if(username === 'in28minutes' && password === 'dummy'){
+      console.log('Success')
+      setShowSeccessMessage(true)
+      setShowErrorMessage(false)
+    }else{
+      setShowSeccessMessage(false)
+      setShowErrorMessage(true)
+      console.log('failed')
+    }
+  }
+
+  function SuccessMessageComponent(){
+    if(showSuccessMessage){
+      return(
+        <div className="successMessage">Authenticated Successfully</div>
+      )
+    }else{
+      return null
+    }
+  }
+
+    function ErrorMessageComponent(){
+      if(showErrorMessage){
+        return(
+          <div className="errorMessage" display={showErrorMessage}>Authentication failed. Please check your credentials.</div>
+        )
+      }else{
+        return null
+      }
+    }
+
   return (
     <div className="Login">
+      <SuccessMessageComponent />
+      <ErrorMessageComponent />
       <div className="LoginForm">
         <div>
           <label>User Name</label>
@@ -35,7 +72,7 @@ function LoginComponent() {
           <input type="password" name="password" value={password} onChange={handlePasswordChange}/>
         </div>
         <div>
-          <button type="button" name="login">Login</button>
+          <button type="button" name="login" onClick={handelSubmit}>Login</button>
         </div>
       </div>
     </div>

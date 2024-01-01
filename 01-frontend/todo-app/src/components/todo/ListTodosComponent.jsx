c8f203e5547bc623ab65d58c8e97df53ba84d377
jeong <chung_junhuck@infonic.co.jp>
2024-01-01 22:33:31 +0900

    get todoList from API

--- 01-frontend/todo-app/src/components/todo/ListTodosComponent.jsx
+++ 01-frontend/todo-app/src/components/todo/ListTodosComponent.jsx
@@ -1,13 +1,27 @@
+import { useEffect, useState } from "react"
+import { retrieveAllTodosForUsername } from "./api/TodoApiService"
+
 export default function ListTodosComponent() {
 
-  const today = new Date()
-  const targetDate = new Date(today.getFullYear() + 12, today.getMonth(), today.getDay())
+  // const today = new Date()
+  // const targetDate = new Date(today.getFullYear() + 12, today.getMonth(), today.getDay())
+  // const todos = [
+  //   {id: 1, description: 'Learn AWS', done: false, targetDate:targetDate},
+  //   {id: 2, description: 'Learn Full Stack Dev', done: false, targetDate:targetDate},
+  //   {id: 3, description: 'Learn DevOps', done: false, targetDate:targetDate}
+  // ]
+  const [todos, setTodos] = useState([])
 
-  const todos = [
-    {id: 1, description: 'Learn AWS', done: false, targetDate:targetDate},
-    {id: 2, description: 'Learn Full Stack Dev', done: false, targetDate:targetDate},
-    {id: 3, description: 'Learn DevOps', done: false, targetDate:targetDate}
-  ]
+  useEffect(() => refreshTodos(), [])
+
+  function refreshTodos() {
+    retrieveAllTodosForUsername('in28minutes')
+    .then(response => {
+      setTodos(response.data)
+    })
+    .catch(error => console.log(error))
+  }
+
   return(
     <div className="container">
       <h1>Things You Want To Do!</h1>
@@ -29,7 +43,8 @@ export default function ListTodosComponent() {
                   <td>{todo.id}</td>
                   <td>{todo.description}</td>
                   <td>{todo.done.toString()}</td>
-                  <td>{todo.targetDate.toDateString()}</td>
+                  {/* <td>{todo.targetDate.toDateString()}</td> */}
+                  <td>{todo.targetDate.toString()}</td>
                 </tr>
               )
             )

37a6fb0633181d95c15a0ce4979c74e7ee7d7de9
jeong <chung_junhuck@infonic.co.jp>
2024-01-03 00:02:17 +0900

    add dependencies:formik, moment. add updateForm:use formik

--- 01-frontend/todo-app/src/components/todo/TodoComponent.jsx
+++ 01-frontend/todo-app/src/components/todo/TodoComponent.jsx
@@ -2,10 +2,12 @@ import { useParams } from 'react-router-dom'
 import { retrieveTodoApi } from './api/TodoApiService'
 import { useAuth } from './security/AuthContext'
 import { useEffect, useState } from 'react'
+import { Formik, Form, Field } from 'formik'
 
 export default function TodoComponent(){
   const {id} = useParams()
-  const[description, setDiscription] = useState('')
+  const[description, setDescription] = useState('')
+  const[targetDate, setTargetDate] = useState('')
   const authContext = useAuth()
   const username = authContext.username
 
@@ -17,16 +19,40 @@ export default function TodoComponent(){
   function retrieveTodos(){
     retrieveTodoApi(username, id)
     .then(response => {
-      setDiscription(response.data.description)
+      setDescription(response.data.description)
+      setTargetDate(response.data.targetDate)
     })
     .catch(error => console.log(error))
+  }
+
+  function onSubmit(values){
+    console.log(values)
   }
 
   return(
     <div className="container">
       <h1>Enter Todo Details</h1>
       <div>
-        description:{description}
+        <Formik initialValues={{ description, targetDate }}
+                enableReinitialize={true}
+                onSubmit={onSubmit}
+        >
+          {
+            (props) => (
+              <Form>
+                <fieldset className="form-group">
+                  <label>Description</label>
+                  <Field type="text" className="form-control" name="description" />
+                  <label>Target Date</label>
+                  <Field type="date" className="form-control" name="targetDate" />
+                </fieldset>
+                <div>
+                  <button className="btn btn-success m-5" type="submit">Save</button>
+                </div>
+              </Form>
+            )
+          }
+        </Formik>
       </div>
     </div>
   )

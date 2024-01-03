f13a0f8cdfa15996d8bf5174158a8270f194b742
jeong <chung_junhuck@infonic.co.jp>
2024-01-03 23:35:14 +0900

    frontside: add updateTodoApi

--- 01-frontend/todo-app/src/components/todo/FooterComponent.jsx
+++
@@ -1,9 +0,0 @@
-export default function FooterComponent() {
-  return (
-    <footer className="footer">
-      <div className="container">
-        2021.04~2023.12.30
-      </div>
-    </footer>
-  )
-}

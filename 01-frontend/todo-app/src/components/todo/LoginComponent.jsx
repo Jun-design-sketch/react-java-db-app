918d48b7c232ce1361b8fb602d1c4141ac61e584
jeong <chung_junhuck@infonic.co.jp>
2024-01-05 23:08:48 +0900

    JWT auth intro

--- 01-frontend/todo-app/src/components/todo/LoginComponent.jsx
+++ 01-frontend/todo-app/src/components/todo/LoginComponent.jsx
@@ -4,7 +4,7 @@ import { useAuth } from './security/AuthContext'
 
 export default function LoginComponent() {
 
-  const [username, setUsername] = useState('username')
+  const [username, setUsername] = useState('in28minutes')
   const [password, setPassword] = useState('')
   const [showErrorMessage, setShowErrorMessage] = useState(false)
   const navigate = useNavigate()
